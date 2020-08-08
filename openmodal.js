825db4804de12c154e0a59dbee7a94b78b205c74
PARKMINJU <dbwk0404@hanmail.com>
2020-08-09 02:41:48 +0900

    menu modal ,js 파일 오타 수정,google계정 modal만들어야 함

--- openmodal.js
+++ openmodal.js
@@ -1,8 +1,16 @@
-const menu_modal = document.querySelector("menu-modal");
+const menuModal = document.querySelector(".menu-modal");
+const overlay = document.querySelector(".modal-overlay");
 const openMenuButton = document.getElementById("open-menu-button");
 
+
 const openModal = () =>{
-    menu_modal.classList.remove("hidden");
+    menuModal.classList.remove("hidden");
+}
+const closeModal = ()=>{
+    menuModal.classList.add("hidden");
 }
 
 openMenuButton.addEventListener("click",openModal);
+overlay.addEventListener("click",closeModal);
+
+
